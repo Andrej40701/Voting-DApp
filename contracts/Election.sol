fbb26aa68cd004f4589b3045525db028acc07f38
Andrej40701 <andrej.stanojkovik@gmail.com>
2023-01-14 16:18:15 +0100

    Added voting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,28 @@ pragma solidity ^0.4.2;
 
 contract Election {
 	
-	string public candidate;
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	mapping(uint => Candidate) public candidates;
+	uint public candidatesCount;
+	mapping(address => bool) public voters;
 	
-	// constructor
 	function Election () public {
-		candidate = "Candidate 1";
+		addCandidate('Candidate 1');
+		addCandidate('Candidate 2');
+	}
+
+	function addCandidate (string _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote (uint _candidateId) public {
+		voters[msg.sender] = true;
+		candidates[_candidateId].voteCount++;
 	}
 }
